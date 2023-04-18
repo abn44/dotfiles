3c4ad75868a7b06b20b7fa605c93a74b381b2693
Alban <alban.biron@gmail.com>
2023-04-18 23:33:19 +0200

    new nvim configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,20 @@
-require("base.keymaps")
-require("base.plugins")
-require("base.plugin_config")
+require("settings")
+require("maps")
+require("plugins")
+
+local themeStatus, gruvbox = pcall(require, "gruvbox")
+
+if themeStatus then
+  --set background=dark " or light if you want light mode
+  vim.cmd("colorscheme gruvbox")
+else
+	return
+end
+
+--local themeStatus, kanagawa = pcall(require, "kanagawa")
+
+--if themeStatus then
+--  vim.cmd("colorscheme kanagawa")
+--else
+--	return
+--end
--- nvim/lua/base/plugin_config/init.lua
+++
@@ -1,6 +0,0 @@
-require("base.plugin_config.gruvbox")
-require("base.plugin_config.lualine")
-require("base.plugin_config.nvim-tree")
-require("base.plugin_config.telescope")
-require("base.plugin_config.treesitter")
-require("base.plugin_config.lsp_config")
